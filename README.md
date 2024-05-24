06edbab6b7b4fa4f956d581950ee20cb5f6ec16e
Someone5685 <yesfriends56@gmail.com>
2024-05-24 15:21:21 -0500

    new file:   LICENSE.txt 	new file:   README.md 	new file:   games/rng.py

---
+++ README.md
@@ -0,0 +1,8 @@
+# Baylor's Projects
+Baylor's Projects is a repository full of python scripts, that I make in my freetime for fun.
+## About me
+Hi, my name is Baylor, and I'm 13 years old. I started this project because one summer, I was thinking, "What am I even gonna do with this much time?" Well, I decided to start learning Python, and a few other languages. After I got the hang of it, I decided I would release my scripts I made for fun to the public. I already knew a little about GitHub, so I figured that'd be a great place to start. And that's when I thought of [baylorprojects.com](https://baylorprojects.com). So I hopped on CloudFlare and registered a domain. And here we are now, writing this.
+## Reporting bugs
+I appreciate your constant support. When you report bugs, it helps me out a ton.
+
+To report a bug, [open an issue](https://github.com/Someone5685/Baylor-Projects/issues), and let me know what the bug is, how to reproduce it, and if it's occurence is widespread. Thank you so much for helping me maintain my projects.
---
+++ games/rng.py
@@ -0,0 +1,35 @@
+# rng.py, The RNG guessing game.
+
+# Copyright (C) Baylor Projects - All Rights Reserved - DO NOT DISTRIBUTE!
+# Unauthorized distribution of this file, via any medium is strictly prohibited
+# Written by Baylor, May 2024
+# COPYLEFT POLICY: Copying is authorized without consent. ALL DERIVATIVES MUST MAINTAIN THE SAME COPYRIGHT! Patent use is prohibited.
+print('\n\nCopyright (C) Baylor Projects - All Rights Reserved - DO NOT DISTRIBUTE!\n')
+
+from random import randint
+while True:
+    print('Welcome to the RNG guessing game.')
+    try:
+        range = int(input('range of numbers to guess: 1-'))
+        tries = int(input('Number of tries you will get: '))
+        trycount = 0
+    except ValueError:
+            print('Please input an integer.')
+            continue
+    break
+num = randint(1,range)
+num = 5
+print(f'I\'m thinking of a number between 1 and {range}. Try to guess it.')
+while tries != 0:
+    while True:
+        try: plr = int(input('Guess: '))
+        except ValueError: 
+            print('Please input an integer.')
+            continue
+        break
+    trycount+1
+    if plr == num:
+        print(f'Congratulations! You guessed my number in {trycount} tries!\nPress \'Enter\' to exit.')
+        input()
+        break
+    break
